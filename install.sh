8cfaafd85a2f146769f9adf97a87d1e24f170ed6
Carlos Gonzalez <contacto.carlosgonzalez@gmail.com>
2019-12-03 16:08:41 -0300

    Update install script and instructions

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 cd SUCHAI-Flight-Software
 echo "Updating SUCHAI-FS"
 git pull
-build_cmd="LINUX RPI --name PlantSat-Chamber --id 6 --hk 0 --node 6 --st_mode 1 --drivers"
+build_cmd="LINUX RPI --name PlantSat-Chamber --id 6 --hk 0 --node 6 --st_mode 2 --drivers"
 echo "Configure (and build) SUCHAI-FS using:"
 echo $build_cmd
 python3 compile.py $build_cmd
