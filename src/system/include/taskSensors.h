0a2de97d538ab5f8e4381689281ffce34f728f26
crojas <camrojas@uchile.cl>
2019-12-03 15:18:16 -0300

    Add Sample Machine to configure step and activation of samples

--- src/system/include/taskSensors.h
+++ src/system/include/taskSensors.h
@@ -17,6 +17,28 @@
 
 #include "repoCommand.h"
 
+
+typedef enum machine_action {
+    ACT_STAND_BY = 0,
+    ACT_START,
+    ACT_PAUSE,
+    ACT_LAST
+} machine_action_t;
+
+typedef enum machine_state {
+    ST_PAUSE = 0,
+    ST_SAMPLING,
+    ST_LAST
+} machine_state_t;
+
+typedef struct sample_machine{
+    machine_state_t state;
+    machine_action_t action;
+    payload_id_t payload;
+    unsigned int step;
+    int samples_left;
+} sample_machine_t;
+
 void taskSensors(void *param);
 
 #endif //_TASKSENSORS_H
