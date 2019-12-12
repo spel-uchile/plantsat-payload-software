30190aaae5fea60c7af079a64361d70a952645da
Alex Becerra <alexbecerra@gmail.com>
2019-12-12 15:06:47 +0000

    Added #defines for default options

--- src/drivers/lp5562/include/lp5562.h
+++ src/drivers/lp5562/include/lp5562.h
@@ -2,7 +2,12 @@
 
 
 #define LP5562_REG_ENABLE		0x00
+#define LP5562_LOG_PWM          0x80
+#define LP5562_CHIP_EN          0x40
+#define LP552_DEFAULT_CHIP_ENABLE (LP5562_CHIP_EN)
+
 #define LP5562_REG_OP_MODE		0x01
+#define LP5562_DEFAULT_OP_MODE  0x00
 
 #define LP5562_REG_B_PWM		0x02
 #define LP5562_REG_G_PWM		0x03
@@ -13,17 +18,26 @@
 #define LP5562_REG_R_CURRENT	0x07
 
 #define LP5562_REG_CONFIG		0x08
-#define LP5562_PWM_HF			0x40
+
+#define LP5562_PWM_256			0x00
+#define LP5562_PWM_558          0x40
+
 #define LP5562_PWRSAVE_EN		0x20
+
 #define LP5562_CLK_INT			0x01	/* Internal clock */
-#define LP5562_DEFAULT_CFG		(LP5562_PWM_HF | LP5562_PWRSAVE_EN)
+#define LP5562_CLK_EXT          0x00
+#define LP5562_CLK_AUTO         0x02
+
+#define LP5562_DEFAULT_CFG		(LP5562_PWM_558 | LP5562_CLK_INT)
 
 #define LP5562_REG_RESET		0x0D
+#define LP5562_RESET            0xFF
 
 #define LP5562_REG_W_PWM		0x0E
 #define LP5562_REG_W_CURRENT	0x0F
 
 #define LP5562_LED_MAP          0x70
+#define LP5562_DEFAULT_MAP      0x00
 
 
 
